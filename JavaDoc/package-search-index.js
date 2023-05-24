85e556eefa58f5ba8cf459fe7e90a56fb4b0355a
cardsandhuskers <cardsandhuskers@gmail.com>
2023-05-24 15:32:35 -0700

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.cardsandhuskers.teams"},{"l":"io.github.cardsandhuskers.teams.commands"},{"l":"io.github.cardsandhuskers.teams.handlers"},{"l":"io.github.cardsandhuskers.teams.listeners"},{"l":"io.github.cardsandhuskers.teams.objects"}];updateSearchResults();
